58d8f62b6f412e086504757ba75c7ec7a0886cdc
OlegKryuchkov <kryuchkov.oleg@gmail.com>
2024-02-02 17:22:01 +0200

    fixed task 4

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,15 +2,15 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-interface PropsT {
+interface Props {
   title: string;
 }
 
-class Component<T extends PropsT> {
+class Component<T> {
   constructor(public props: T) {}
 }
 
-class Page extends Component<{ title: string }> {
+class Page extends Component<Props> {
   pageInfo() {
     console.log(this.props.title);
   }
